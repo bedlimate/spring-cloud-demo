785f61fca59b2d9c07bc9341bbf7ce7eae35ecfb
liuyun <1021930234@!qq.com>
2018-07-10 22:27:21 +0800

    添加注释说明

--- spring-cloud-hystrix/src/main/java/com/liuyun/springcloudhystrix/config/DefaultConfig.java
+++ spring-cloud-hystrix/src/main/java/com/liuyun/springcloudhystrix/config/DefaultConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class DefaultConfig {
 
+    //此处必须加上@LoadBalanced这个注解，因为在RestTemplate调用的时候，
+    // 为了不具体依赖于某一服务，使用了以服务名称代替服务的URL，如果不加上这个注解，后续在发起真实http请求的时候
+    // 无法将服务名称替换为真实的服务URL，从而会跑出无效的URL异常
+
     @Bean
     @LoadBalanced
     public RestTemplate newRestTemplate(){
