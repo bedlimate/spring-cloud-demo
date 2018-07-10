afa338809c02e3ab110ae3cc897099941a592964
liuyun <1021930234@!qq.com>
2018-07-11 00:28:01 +0800

    在feign客户端中启用hystrix

--- feign/src/main/java/com/liuyun/feign/FeignApplication.java
+++ feign/src/main/java/com/liuyun/feign/FeignApplication.java
@@ -2,13 +2,13 @@ package com.liuyun.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients
-@EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class FeignApplication {
 
     public static void main(String[] args) {
--- feign/src/main/java/com/liuyun/feign/api/IHelloService.java
+++ feign/src/main/java/com/liuyun/feign/api/IHelloService.java
@@ -1,12 +1,22 @@
 package com.liuyun.feign.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("service-1")
+@FeignClient(value = "service-1", fallback = IHelloService.HelloServiceFallback.class)
 public interface IHelloService {
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHello(@RequestParam("name") String name);
+
+    @Component
+    class HelloServiceFallback implements IHelloService{
+
+        @Override
+        public String sayHello(String name) {
+            return "This is an error from fallback in feign client!";
+        }
+    }
 }
