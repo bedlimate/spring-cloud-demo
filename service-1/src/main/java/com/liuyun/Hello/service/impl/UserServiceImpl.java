a7a038ca6ffb33f5dff08562353546adf6de5ff9
liuyun <1021930234@!qq.com>
2018-07-16 23:41:02 +0800

    真实保存数据到数据库

--- service-1/src/main/java/com/liuyun/Hello/service/impl/UserServiceImpl.java
+++ service-1/src/main/java/com/liuyun/Hello/service/impl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.liuyun.Hello.service.impl;
 
+import com.liuyun.Hello.mapper.UserMapper;
 import com.liuyun.Hello.service.IUserService;
 import com.liuyun.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements IUserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public void saveUser(User user) {
-        System.out.println("save user:[ " + user + "] to db succeed!" );
+        userMapper.saveUser(user);
     }
 }
