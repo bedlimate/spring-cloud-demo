1271d1b6993aaebfed238218eebcdd92fd5768c7
liuyun <1021930234@!qq.com>
2018-07-22 14:46:52 +0800

    对外提供批量查询的接口

--- service-1/src/main/java/com/liuyun/Hello/controller/UserController.java
+++ service-1/src/main/java/com/liuyun/Hello/controller/UserController.java
@@ -3,9 +3,9 @@ package com.liuyun.Hello.controller;
 import com.liuyun.Hello.service.IUserService;
 import com.liuyun.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,5 +16,11 @@ public class UserController {
     @PutMapping("/user")
     public void saveUser(@RequestBody User user){
         userService.saveUser(user);
+    }
+
+    // GET http://localhost:8011/users?userId=100,200,300
+    @GetMapping("/users")
+    public List<User> getUsers(@RequestParam List<Integer> userIds){
+        return userService.getUsers(userIds);
     }
 }
