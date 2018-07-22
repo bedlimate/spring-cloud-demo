dee7f7e39577ca5f2bb7b4e467a9113709f8567c
liuyun <1021930234@!qq.com>
2018-07-22 14:32:17 +0800

    增加参数校验

--- service-1/src/main/java/com/liuyun/Hello/service/impl/UserServiceImpl.java
+++ service-1/src/main/java/com/liuyun/Hello/service/impl/UserServiceImpl.java
@@ -6,6 +6,10 @@ import com.liuyun.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class UserServiceImpl implements IUserService {
 
@@ -16,4 +20,16 @@ public class UserServiceImpl implements IUserService {
     public void saveUser(User user) {
         userMapper.saveUser(user);
     }
+
+    @Override
+    public List<User> getUsers(List<Integer> userIds) {
+        userIds = Optional.ofNullable(userIds)
+                .orElseThrow(() -> new IllegalArgumentException("userIds is null"))
+                .stream().filter(userId -> userId != null)
+                .collect(Collectors.toList());
+
+
+        return userMapper.getUsers(userIds);
+    }
+
 }
